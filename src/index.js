73e83dcd5a5327854c0e0ab9dc36b9d6061af715
Nezeri <ichigvintsev@icloud.com>
2026-03-17 18:06:11 +0500

    duck and dog

--- src/index.js
+++ src/index.js
@@ -29,28 +29,43 @@ function getCreatureDescription(card) {
 
 
 
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+
+class Duck extends Card {
+    constructor() {
+        super("Мирная утка", 2);
+    }
+
+    quacks() {
+         console.log('quack') 
+    }
+
+    swims() {
+         console.log('float: both;') 
+    }
 }
 
-
-// Основа для собаки.
-function Dog() {
+function isDuck(obj){
+    return obj instanceof Duck;
 }
 
+function isDog(obj){
+    return obj instanceof Dog;
+}
+
+class Dog extends Card {
+    constructor() {
+        super("Пес-бандит", 3);
+    }
+}
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
-
-// Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
